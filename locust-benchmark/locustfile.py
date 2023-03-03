22f3fe86bd6bed7208ea700ddb267b18c87b6c9b
ciphersmaug <39407479+ciphersmaug@users.noreply.github.com>
2023-03-03 11:08:42 +0100

    Finalized the initial benchmarking on my minikube

--- locust-benchmark/locustfile.py
+++ locust-benchmark/locustfile.py
@@ -8,16 +8,17 @@ class WebTasks(TaskSet):
 
     @task
     def load(self):
-        base64string = base64.b64encode("user:password".encode()) #dXNlcjpwYXNzd29yZA==
+        #base64string = base64.b64encode("user:password".encode()) #dXNlcjpwYXNzd29yZA==
 
-        catalogue = self.client.get("/catalogue").json()
-        category_item = choice(catalogue)
-        item_id = category_item["id"]
+        #catalogue = self.client.get("/catalogue").json()
+        #category_item = choice(catalogue)
+        #item_id = category_item["id"]
 
-        self.client.get("/")
+        #self.client.get("/")
         self.client.get("/login", headers={"Authorization":"Basic dXNlcjpwYXNzd29yZA=="})#"Basic %s" % base64string})
-        self.client.get("/category.html")
-        self.client.get("/detail.html?id={}".format(item_id))
+        self.client.get("/customers")
+        self.client.get("/cards")
+        self.client.get("/addresses")
         #self.client.delete("/cart")
         #self.client.post("/cart", json={"id": item_id, "quantity": 1})
         #self.client.get("/basket.html")
